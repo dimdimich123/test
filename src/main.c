ce3b4f74b4e4d2031e833ec2952ed8b80f50b391
dimdimich123 <Frostm333@gmail.com>
2019-05-29 15:40:52 +0700

    finish

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main()
     Vector2i mousexy;
     srand(time(NULL));
     Image backIM;
-    backIM.loadFromFile("../src/images/wood.png");
+    backIM.loadFromFile("src/images/wood.png");
 
     Texture backTX;
     backTX.loadFromImage(backIM);
@@ -20,7 +20,7 @@ int main()
     backSP.setPosition(0, 0);
 
     Font font;
-    font.loadFromFile("../src/font/16872.ttf");
+    font.loadFromFile("src/font/16872.ttf");
     Text text, t1, t2, t3, t4;
     text.setFont(font);
     t1.setFont(font);
